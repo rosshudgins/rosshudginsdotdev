c52c84d781b08abda8a3836ea4ec249df34781db
Ross Hudgins <michael.ross.hudgins@gmail.com>
2020-04-26 17:07:35 -0500

    title and url

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://rosshudgins.dev',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by Ross Hudgins',
+  subtitle: '',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
